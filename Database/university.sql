e2c1ce16b11030cb18bfcc62cbd00ec87ebd05a6
RyzhikovD <ryzhikov.dmvl@gmail.com>
2019-08-19 15:47:58 +0300

    Edited database

--- Database/university.sql
+++ Database/university.sql
@@ -14,10 +14,10 @@ CREATE TABLE IF NOT EXISTS `students` (
   `dateOfEnrollment` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`student_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+COMMIT;
 
 INSERT INTO `students` (`studentId`, `faculty`, `groupNumber`, `studentName`, `scholarship`, `dateOfEnrollment`) VALUES
 (1, 'ICIS', 'hello@gmail.com', 'Alex', 'usual', '01.09.2016'),
 (2, 'ICIS', 'dolly@gmail.com', 'Dolly', 'usual', '01.09.2016'),
 (3, 'ILPT', 'sonoo@gmail.com', 'Sonoo', 'increased', '01.09.2017');
-
 COMMIT;
